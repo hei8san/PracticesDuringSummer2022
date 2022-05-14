7afa1bbe970ec080fb53ecc66e424e35c1d2616a
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-14 16:28:10 -0700

    finish

--- LeetCode88/src/Main.java
+++ LeetCode88/src/Main.java
@@ -2,7 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		Solution solution = new Solution();
+		int[] nums1 = {1,2,3,0,0,0};
+		int m = 3;
+		int[] nums2 = {2,5,6};
+		int n = 3;
+		
+		solution.merge(nums1, m, nums2, n);
 
 	}
 
--- LeetCode88/src/Solution.java
+++ LeetCode88/src/Solution.java
@@ -1,4 +1,37 @@
-
 public class Solution {
+//	public void merge(int[] nums1, int m, int[] nums2, int n) {
+//		int[] combinedArray = new int[m + n];
+//		int index = 0;
+//		for (int i = 0; i < m; i++) {
+//			combinedArray[i] = nums1[i];
+//			index++;
+//			
+//		}
+//		for (int i = 0; i < n; i++) {
+//			combinedArray[index] = nums2[i];
+//			index++;
+//			
+//		}
+//		Arrays.sort(combinedArray);
+//		for(int i = 0; i < m+n; i++) {
+//			nums1[i] = combinedArray[i];
+//		}
+//		
+//		System.out.println(Arrays.toString(nums1));
+//
+//	}
+	
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int p1 = m-1, p2 = n-1, i = m+n-1;
+		
+		while(p2 >=0) {
+			if(p1 >= 0 && nums1[p1] > nums2[p2]) {
+				nums1[i--] = nums1[p1--];
+			}
+			else {
+				nums1[i--] = nums2[p2--];
+			}
+		}
+	}
 
 }
