6467bc98e86e443a6ccbda8644538b934915cced
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-15 13:19:52 -0700

    2nd

---
+++ LeetCode350/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int[] nums1 = {1,9,2,8,7};
+		int[] nums2 = {1,7,6,5,3};
+		System.out.println(Arrays.toString(solution.intersect(nums1, nums2)));
+
+	}
+
+}
---
+++ LeetCode350/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Solution {
+	public int[] intersect(int[] nums1, int[] nums2) {
+		int length1 = nums1.length;
+		int length2 = nums2.length;
+		int[] ans = new int[length1 + length2];
+		int i = 0, j = 0, k = 0;
+		
+		Arrays.sort(nums1);
+		Arrays.sort(nums2);
+		
+		while(i < length1 && j < length2) {//indexOutOfBoundを避けるため
+			if(nums1[i] < nums2[j]) {
+				i++;
+			}
+			else if (nums1[i] > nums2[j]) {
+				j++;
+			} else {
+				ans[k++] = nums1[i++];
+				j++;
+			}
+		}
+		return Arrays.copyOfRange(ans, 0, k);
+	}
+	
+//	public int[] intersect(int[] nums1, int[] nums2) {
+//		
+//	}
+
+}
