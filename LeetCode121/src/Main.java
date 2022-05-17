d45827b53b2b7ec74fe93c503132ffe70d690453
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-17 15:45:35 -0700

    2

--- LeetCode121/src/Main.java
+++ LeetCode121/src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		Solution solution = new Solution();
+		
 
 	}
 
--- LeetCode121/src/Solution.java
+++ LeetCode121/src/Solution.java
@@ -1,4 +1,7 @@
 
 public class Solution {
+public int maxProfit(int[] prices) {
+        
+    }
 
 }
