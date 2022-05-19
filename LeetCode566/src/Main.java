1f8ca4b3177ffd1e8fb8d785bc193ad754c54212
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-19 12:31:36 -0700

    finished

--- LeetCode566/src/Main.java
+++ LeetCode566/src/Main.java
@@ -1,16 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int[][] mat = {{1,2},{3,4}};
-		int r = 1, c= 4;
+		int[][] mat = {{1,2,3},{4,5,6}};
+		int r = 3, c= 2;
 		Solution solution  = new Solution();
 		int[][] ans = solution.matrixReshape(mat, r, c);
-		for(int i = 0; i < ans.length; i++) {
-			for(int j = 0; j < ans[i][j]; j++) {
-				System.out.print(ans[i][j]);
-			}
-			System.out.println();
-		}
+		System.out.println(Arrays.deepToString(ans));
 		
 
 	}
--- LeetCode566/src/Solution.java
+++ LeetCode566/src/Solution.java
@@ -1,25 +1,25 @@
 
 public class Solution {
-public int[][] matrixReshape(int[][] mat, int r, int c) {
-		
-        
+	public int[][] matrixReshape(int[][] mat, int r, int c) {
+
 		int m = mat.length;
 		int n = mat[0].length;
 
-        if(m*n != r*c) return mat;
+		if (m * n != r * c)
+			return mat;// もし引数のサイズと出力するサイズが合わないとき
 
 		int[][] ans = new int[r][c];
-		int row_mat = 0;
-		int col_mat = 0;
+		int row_ans = 0; // 出力用の行
+		int col_ans = 0; // 出力用の列
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
-				ans[row_mat][col_mat] = mat[i][j];
+				ans[row_ans][col_ans] = mat[i][j];
 
-				col_mat++;
+				col_ans++;
 
-				if (col_mat == c) {
-					col_mat = 0;
-					row_mat++;
+				if (col_ans == c) {
+					col_ans = 0;
+					row_ans++;
 				}
 
 			}
