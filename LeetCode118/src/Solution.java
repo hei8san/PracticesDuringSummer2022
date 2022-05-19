544ac848c8a6f25cbefda78f6953734646b18420
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-19 16:08:21 -0700

    test

--- LeetCode118/src/Solution.java
+++ LeetCode118/src/Solution.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
+	public List<List<Integer>> generate(int numRows) {
+		List<List<Integer>> ans = new ArrayList<>();
+		
+		if(numRows == 0) return ans;
+		
+		List<Integer> first_row = new ArrayList<>();
+		first_row.add(1);
+		ans.add(first_row);
+		
+		for(int i = 1; i < numRows; i++) {
+			List<Integer> prev_row = ans.get(i-1);
+			List<Integer> current_row = new ArrayList<>();
+			
+			current_row.add(1);
+			for(int j = 1; j < i; j++) {
+				current_row.add(prev_row.get(j-1) + prev_row.get(j));
+				
+			}
+			current_row.add(1);
+			ans.add(current_row);
+		}
+		return ans;
+		
+	}
 	
 //	public List<List<Integer>> generate(int numRows) {
 //		List<List<Integer>> ans = new LinkedList<List<Integer>>();
