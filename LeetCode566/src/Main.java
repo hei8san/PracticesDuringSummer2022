3d519c627c7cd8e320d24effee19e9d69737923b
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-18 17:13:46 -0700

    2nd not fnished yet. review tomorrow.

--- LeetCode566/src/Main.java
+++ LeetCode566/src/Main.java
@@ -1,8 +1,17 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		int[][] mat = {{1,2},{3,4}};
+		int r = 1, c= 4;
+		Solution solution  = new Solution();
+		int[][] ans = solution.matrixReshape(mat, r, c);
+		for(int i = 0; i < ans.length; i++) {
+			for(int j = 0; j < ans[i][j]; j++) {
+				System.out.print(ans[i][j]);
+			}
+			System.out.println();
+		}
+		
 
 	}
 
--- LeetCode566/src/Solution.java
+++ LeetCode566/src/Solution.java
@@ -1,4 +1,32 @@
 
 public class Solution {
+public int[][] matrixReshape(int[][] mat, int r, int c) {
+		
+        
+		int m = mat.length;
+		int n = mat[0].length;
 
+        if(m*n != r*c) return mat;
+
+		int[][] ans = new int[r][c];
+		int row_mat = 0;
+		int col_mat = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				ans[row_mat][col_mat] = mat[i][j];
+
+				col_mat++;
+
+				if (col_mat == c) {
+					col_mat = 0;
+					row_mat++;
+				}
+
+			}
+
+		}
+
+		return ans;
+
+	}
 }
