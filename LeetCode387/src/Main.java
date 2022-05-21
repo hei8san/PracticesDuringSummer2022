c5754c1fa5f610e43d361ad427ce3bd16dbf4a2f
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-20 17:05:36 -0700

    2

--- LeetCode387/src/Main.java
+++ LeetCode387/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Solution s = new Solution();
 		String str = "leetcode";
-		
+		System.out.println(s.firstUniqChar(str));
 
 	}
 
--- LeetCode387/src/Solution.java
+++ LeetCode387/src/Solution.java
@@ -1,6 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
-public int firstUniqChar(String s) {
-        
-    }
+	public int firstUniqChar(String s) {
+		Map<Character, Integer> input = new HashMap<>();
+		
+		for (int i = 0; i < s.length(); i++) {
+			char current = s.charAt(i);
+			if (!input.containsKey(current)) {
+				input.put(current, i);
+			} else {
+				input.put(current, -1);
+			}
+		}
+
+		int min = Integer.MAX_VALUE;
+		for (char c : input.keySet()) {
+			if (input.get(c) != -1) {
+				min = Math.min(input.get(c), min);
+			}
+		}
+		
+		return min == Integer.MAX_VALUE ? -1 : min;
+		
+
+	}
 }
