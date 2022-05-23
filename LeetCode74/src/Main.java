9121d1d1606b24f7f0a795ae43164d6bdd1988be
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-23 15:23:12 -0700

    review

--- LeetCode74/src/Main.java
+++ LeetCode74/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Solution s = new Solution();
+		int[] sortedArr = { 1, 3, 5, 8, 20, 45, 75, 82, 8765, 86547 };
+		System.out.println(s.binarySearch(sortedArr, 82));
 
 	}
 }
--- LeetCode74/src/Solution.java
+++ LeetCode74/src/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
 
 		while (left <= right) {
 			int midpoint = left + ((right - left) / 2);
-			int midpoint_element = matrix[midpoint / columns][midpoint%columns];
+			int midpoint_element = matrix[midpoint / columns][midpoint % columns];
 			if (midpoint_element == target) {
 				return true;
 			} else if (midpoint_element > target) {
@@ -20,6 +20,31 @@ public class Solution {
 			} else if (midpoint_element < target) {
 				left = midpoint + 1;
 			}
+		}
+		return false;
+	}
+
+	public boolean searchMatrixTest(int[][] matrix, int target) {
+		int rows = matrix.length;
+		int columns = matrix[0].length;
+
+		int left = 0;
+		int right = (rows * columns) - 1;
+
+		if (rows == 0)
+			return false;
+
+		while (left <= right) {
+			int midpoint = left + ((right - left) / 2);
+			int mid_element = matrix[midpoint / columns][midpoint % columns];
+			if (mid_element == target) {
+				return true;
+			} else if (mid_element > target) {
+				right = midpoint - 1;
+			} else if (mid_element < target) {
+				left = midpoint + 1;
+			}
+
 		}
 		return false;
 	}
