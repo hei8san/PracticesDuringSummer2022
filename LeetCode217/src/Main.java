6b051b740a1a1b541f24594f8fb0bb6bf1e12db5
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-23 17:45:35 -0700

    1

---
+++ LeetCode217/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int nums[] = {1,2,3,1};
+		System.out.println(s.containsDuplicate(nums));
+
+	}
+
+}
