66ff5bc795efc61d0f7e4d25f448e78534925baa
hamuh <hamuh@DESKTOP-HIMU1I2>
2022-05-24 17:08:51 -0700

    review

--- LeetCode1/src/Main.java
+++ LeetCode1/src/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		int[] nums = { 3,2,4 };
+		int[] nums = { 3,2,3 };
 		Solution solution = new Solution();
-		System.out.println(Arrays.toString(solution.twoSum(nums, 6)));
+		System.out.println(Arrays.toString(solution.twoSumTest(nums, 6)));
 
 	}
 
--- LeetCode1/src/Solution.java
+++ LeetCode1/src/Solution.java
@@ -40,5 +40,39 @@ public class Solution {
 		throw new IllegalArgumentException();
 	}
 	
+	
+	public int[] twoSumTest(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>(); //Key(element), Value(index)
+		int ans[] = new int[2];
+		for(int i  = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if(map.containsKey(complement)) {
+				ans[0] = map.get(complement);
+				ans[1] = i;
+			} else {
+				map.put(nums[i], i);
+				System.out.println(map.toString());
+			}
+			
+		}
+		
+		return ans;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
